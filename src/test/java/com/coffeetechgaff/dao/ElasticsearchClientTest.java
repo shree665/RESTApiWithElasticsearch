42ad25a8716970f673de5d94cab88e5e45c3dff2
Vivek Subedi <shree665@gmail.com>
2017-09-02 12:35:14 -0400

    added logging

---
+++ src/test/java/com/coffeetechgaff/dao/ElasticsearchClientTest.java
@@ -0,0 +1,25 @@
+package com.coffeetechgaff.dao;
+
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ElasticsearchClientTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
